23c2e990003d5887f974ca6dbb13e958d952b1a5
amcmullan <40965393+amcmullan@users.noreply.github.com>
2018-07-17 12:35:33 -0400

    Create dice.js
    first draft

---
+++ dice.js
@@ -0,0 +1,27 @@
+
+//allow user input, using readline apparently. because this is node.js.
+var readline = require('readline');
+
+var rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
+//create a function to randomize a number between 1 and input
+function randomDice(num) {
+	var dice = Math.floor(Math.random() * num + 1); 
+	return;
+	
+}
+
+/* ask the user what size dice they want to roll, check if it is zero or less, to avoid confusing answers. then return the result and close. */
+rl.question("What is the maximum of the dice you would like to roll? ", (answer) => {
+	if (answer > 0) {
+		var dice = Math.floor(Math.random() * answer + 1);
+	console.log("you rolled a... ", dice);
+
+rl.close();
+	} else {
+		console.log("the result is zero or less... are you trying to break me?");
+		rl.close();
+	}
+});
